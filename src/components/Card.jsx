cc4af7280bce54dc27c0d18f0e126d44390a3eb4
Mariana <marsmolina20@gmail.com>
2023-07-18 19:08:01 +0200

    changed url of images

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ export default function Card({image, selected, onClick}) {
       <div className={selected && 'selected'}>
 
         <img src={image} alt=""  className="card-face" />
-        <img src={`${process.env.PUBLIC_URL}/assets/cat.jpg`} alt="" onClick={onClick} className="card-back"/>
+        <img src={'https://images.pexels.com/photos/1715092/pexels-photo-1715092.jpeg?auto=compress&cs=tinysrgb&w=600'} alt="" onClick={onClick} className="card-back"/>
       </div>
     </div>
   )
--- src/utilities/shuffle.js
+++ src/utilities/shuffle.js
@@ -1,13 +1,29 @@
 const shuffle = () => {
 	const assets = [
-		{ image: `${process.env.PUBLIC_URL}/assets/dog1.jpg` },
-		{ image: `${process.env.PUBLIC_URL}/assets/dog2.jpg` },
-		{ image: `${process.env.PUBLIC_URL}/assets/dog3.jpg` },
-		{ image: `${process.env.PUBLIC_URL}/assets/dog4.jpg` },
-		{ image: `${process.env.PUBLIC_URL}/assets/dog5.jpg` },
-		{ image: `${process.env.PUBLIC_URL}/assets/dog6.jpg` },
-		{ image: `${process.env.PUBLIC_URL}/assets/dog7.jpg` },
-		{ image: `${process.env.PUBLIC_URL}/assets/dog8.jpg` },
+		{
+			image: `https://images.pexels.com/photos/14531531/pexels-photo-14531531.jpeg?auto=compress&cs=tinysrgb&w=600`,
+		},
+		{
+			image: `https://images.pexels.com/photos/13085125/pexels-photo-13085125.jpeg?auto=compress&cs=tinysrgb&w=600`,
+		},
+		{
+			image: `https://images.pexels.com/photos/7752793/pexels-photo-7752793.jpeg?auto=compress&cs=tinysrgb&w=600`,
+		},
+		{
+			image: `https://images.pexels.com/photos/17407385/pexels-photo-17407385.jpeg?auto=compress&cs=tinysrgb&w=600`,
+		},
+		{
+			image: `https://images.pexels.com/photos/40986/dog-bulldog-white-tongue-40986.jpeg?auto=compress&cs=tinysrgb&w=600`,
+		},
+		{
+			image: `https://images.pexels.com/photos/15824643/pexels-photo-15824643/free-photo-of-madera-naturaleza-animal-perro.png?auto=compress&cs=tinysrgb&w=600`,
+		},
+		{
+			image: `https://images.pexels.com/photos/8895723/pexels-photo-8895723.jpeg?auto=compress&cs=tinysrgb&w=600`,
+		},
+		{
+			image: `https://images.pexels.com/photos/5321441/pexels-photo-5321441.jpeg?auto=compress&cs=tinysrgb&w=600`,
+		},
 	];
 	return [...assets, ...assets]
 		.sort(() => Math.random() - 0.5)
