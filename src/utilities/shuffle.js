3b6270a9741751ef6167e73fea035faec2dffd14
Mariana <marsmolina20@gmail.com>
2023-07-18 17:18:56 +0200

    changed image path

--- src/utilities/shuffle.js
+++ src/utilities/shuffle.js
@@ -1,13 +1,13 @@
 const shuffle = () => {
 	const assets = [
-		{ image: '/assets/dog1.jpg' },
-		{ image: '/assets/dog2.jpg' },
-		{ image: '/assets/dog3.jpg' },
-		{ image: '/assets/dog4.jpg' },
-		{ image: '/assets/dog5.jpg' },
-		{ image: '/assets/dog6.jpg' },
-		{ image: '/assets/dog7.jpg' },
-		{ image: '/assets/dog8.jpg' },
+		{ image: './assets/dog1.jpg' },
+		{ image: './assets/dog2.jpg' },
+		{ image: './assets/dog3.jpg' },
+		{ image: './assets/dog4.jpg' },
+		{ image: './assets/dog5.jpg' },
+		{ image: './assets/dog6.jpg' },
+		{ image: './assets/dog7.jpg' },
+		{ image: './assets/dog8.jpg' },
 	];
 	return [...assets, ...assets]
 		.sort(() => Math.random() - 0.5)
